ce0765d6970e93121b965d09627ecc2daaf37571
wutao <wutao@rightpaddle.com>
2019-04-19 15:24:08 +0800

    test

--- cpp/agora_api.cpp
+++ cpp/agora_api.cpp
@@ -3,19 +3,9 @@
 #include "agora_api.h"
 #include "record.h"
 using namespace std;
-#ifdef __cplusplus
-extern "C" {
-#endif
+
 
 AGORA_API RecordingEnginex* CreateRecordingEngine(){
    return (RecordingEnginex*)new RecordingEngine();
 }
 
-AGORA_API void SetOnErrorEventHandler(RecordingEnginex* handler, void* onError){
-   ((RecordingEngine*)handler)->SetonError((ponError)onError);
-}
-
-
-#ifdef __cplusplus
-};
-#endif
--- engine.go
+++ engine.go
@@ -1,25 +1,22 @@
-package agora_record_go
+package main
 
 /*
-#cgo CFLAGS: -I${SRCDIR}/cpp
 #cgo LDFLAGS: -L/usr/local/lib  -L${SRCDIR}/cpp -lRecordEngine -lmyagora
 
-#include <stdlib.h>
 #include "cpp/agora_api.h"
-//#include <rocketmq/CPushConsumer.h>
 
-//extern int consumeMessageCallback(CPushConsumer *consumer, CMessageExt *msg);
-//
-//int callback_cgo(CPushConsumer *consumer, CMessageExt *msg) {
-//	return consumeMessageCallback(consumer, msg);
-//}
 */
 import "C"
 
-type AgoraRecord struct{
+type AgoraRecord struct {
 	RecordingEnginex *C.struct_RecordingEnginex
 }
 
-func (ar *AgoraRecord) NewRecordingEnginex(){
+func (ar *AgoraRecord) NewRecordingEnginex() {
 	ar.RecordingEnginex = C.CreateRecordingEngine()
+}
+
+func main() {
+	ag := AgoraRecord{}
+	ag.NewRecordingEnginex()
 }
